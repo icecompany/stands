6cee73b3de17bf000c99c1b6814c321cf923093b
xakepok@xakepok.com <44duP_yx579>
2020-04-28 22:40:41 +0300

    Catalog model

---
+++ administrator/components/com_stands/models/catalog.php
@@ -0,0 +1,83 @@
+<?php
+defined('_JEXEC') or die;
+use Joomla\CMS\MVC\Model\AdminModel;
+
+class PricesModelCatalog extends AdminModel {
+
+    public function getItem($pk = null)
+    {
+        return parent::getItem($pk);
+    }
+
+    public function save($data)
+    {
+        return parent::save($data);
+    }
+
+    public function getTable($name = 'Catalogs', $prefix = 'TableStands', $options = array())
+    {
+        return JTable::getInstance($name, $prefix, $options);
+    }
+
+    public function getForm($data = array(), $loadData = true)
+    {
+        $form = $this->loadForm(
+            $this->option.'.catalog', 'catalog', array('control' => 'jform', 'load_data' => $loadData)
+        );
+        if (empty($form))
+        {
+            return false;
+        }
+
+        return $form;
+    }
+
+    protected function loadFormData()
+    {
+        $data = JFactory::getApplication()->getUserState($this->option.'.edit.catalog.data', array());
+        if (empty($data))
+        {
+            $data = $this->getItem();
+        }
+
+        return $data;
+    }
+
+    protected function prepareTable($table)
+    {
+        $all = get_class_vars($table);
+        unset($all['_errors']);
+        $nulls = []; //Поля, которые NULL
+        foreach ($all as $field => $v) {
+            if (empty($field)) continue;
+            if (in_array($field, $nulls)) {
+                if (!strlen($table->$field)) {
+                    $table->$field = NULL;
+                    continue;
+                }
+            }
+            if (!empty($field)) $table->$field = trim($table->$field);
+        }
+
+        parent::prepareTable($table);
+    }
+
+    protected function canEditState($record)
+    {
+        $user = JFactory::getUser();
+
+        if (!empty($record->id))
+        {
+            return $user->authorise('core.edit.state', $this->option . '.catalog.' . (int) $record->id);
+        }
+        else
+        {
+            return parent::canEditState($record);
+        }
+    }
+
+    public function getScript()
+    {
+        return 'administrator/components/' . $this->option . '/models/forms/catalog.js';
+    }
+}

3dcb5231230d679aec2fa840c96253ea01bb342b
Anton <asharikov@icecompany.org>
2020-06-19 11:36:37 +0300

    Busy stands is not available

--- administrator/components/com_stands/models/fields/stand.php
+++ administrator/components/com_stands/models/fields/stand.php
@@ -19,16 +19,38 @@ class JFormFieldStand extends JFormFieldList
             ->from("`#__mkv_stands` s")
             ->leftJoin("#__mkv_stand_pavilions p on p.id = s.pavilionID")
             ->order("s.number");
-
+        //Получаем ID стенда или сразу сделки
+        $input = JFactory::getApplication()->input;
+        $contractStandID = $input->getInt('id', 0);
+        JTable::addIncludePath(JPATH_ADMINISTRATOR . "/components/com_contracts/tables");
+        if ($contractStandID > 0) {
+            $table = JTable::getInstance('Stands', 'TableContracts');
+            $table->load($contractStandID);
+            $contractID = $table->contractID;
+            $standID = $table->standID;
+        }
+        else {
+            $contractID = JFactory::getApplication()->getUserState("com_stands.stand.contractID");
+        }
+        //Получаем ID проекта
+        $table = JTable::getInstance('Contracts', 'TableContracts');
+        $table->load($contractID);
+        $projectID = $table->projectID;
+        //Получаем ID каталога стендов для проекта
         JTable::addIncludePath(JPATH_ADMINISTRATOR . "/components/com_prj/tables");
         $table = JTable::getInstance('Projects', 'TablePrj');
-        $project = PrjHelper::getActiveProject();
-        if (is_numeric($project)) {
-            $table->load($project);
-            if (is_numeric($table->catalogID)) {
-                $query->where("s.catalogID = {$table->catalogID}");
-            }
-        }
+        $table->load($projectID);
+        $catalogID = $table->catalogID;
+        $query->where("s.catalogID = {$catalogID}");
+
+        //Отсекаем занятые стенды
+        JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . "/components/com_contracts/models", "ContractsModel");
+        $model = JModelLegacy::getInstance("StandsLight", "ContractsModel", ['projectID' => $projectID, 'byProjectID' => true]);
+        $items = $model->getItems();
+        $ids = array_keys($items);
+        $ids = implode(', ', $ids);
+        $query->where("s.id not in ({$ids})");
+        if ($contractStandID > 0) $query->orWhere("s.id = {$db->q($standID)}");
 
         $result = $db->setQuery($query)->loadObjectList();
 

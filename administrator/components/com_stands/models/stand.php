590fd07cff52b270c3c6f258f68ee34d1dfb397d
asharikov <asharikov@icecompany.org>
2021-03-26 14:13:03 +0300

    Update stand square in contract items

--- administrator/components/com_stands/models/stand.php
+++ administrator/components/com_stands/models/stand.php
@@ -9,9 +9,11 @@ class StandsModelStand extends AdminModel {
         return parent::getItem($pk);
     }
 
-    public function save($data)
+    public function save($data): bool
     {
-        return parent::save($data);
+        $s = parent::save($data);
+        if ($s && $data['id'] !== null) $this->updateSquareItems($data['id'], $data['square']);
+        return $s;
     }
 
     public function getTable($name = 'Stands', $prefix = 'TableStands', $options = array())
@@ -42,6 +44,56 @@ class StandsModelStand extends AdminModel {
         }
 
         return $data;
+    }
+
+    /**
+     * Обновляет значение площади стенда в заказанных услугах, к которым этот стенд привязан
+     * @param int $standID
+     * @param float $square
+     *
+     * @since version 2.0.4
+     */
+    private function updateSquareItems(int $standID, float $square)
+    {
+        $ids = $this->getSquareItemIDs($standID);
+        if (empty($ids)) return;
+        $db = JFactory::getDbo();
+        $db->setQuery("set @is_zero = 0")->execute();
+        $query = $db->getQuery(true);
+        $query
+            ->update("#__mkv_contract_items")
+            ->set($db->qn('value') . " = " .$db->q($square));
+        if (count($ids) > 1) {
+            $list = implode(', ', $ids);
+            $query->where("id in ({$list})");
+        }
+        else {
+            $query->where("id = {$db->q($ids[0])}");
+        }
+        $db->setQuery($query)->execute();
+        $db->setQuery("set @is_zero = 1")->execute();
+    }
+
+    /**
+     * Возвращает ID заказанных позиций экспомест, которые нужно поменять в сделках, в которых заказан этот стенд
+     * @param int $standID
+     *
+     * @return array
+     *
+     * @since version 2.0.4
+     */
+    private function getSquareItemIDs(int $standID): array
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query
+            ->select("ci.id")
+            ->from("#__mkv_contract_stands cs")
+            ->leftJoin("#__mkv_contract_items ci on ci.contractStandID = cs.id")
+            ->leftJoin("#__mkv_price_items pi on pi.id = ci.itemID")
+            ->where("cs.standID = {$db->q($standID)}")
+            ->where("pi.square_type in (1, 2, 3, 4, 5, 6)");
+        return $db->setQuery($query)->loadColumn() ?? [];
     }
 
     protected function prepareTable($table)
